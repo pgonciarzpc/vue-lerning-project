a13c819b8805c53a32313e9cb1c6861c1e6bbba9
Paweł <pgonciarzpc@gmail.com>
2018-02-28 18:56:26 +0100

    Lesson finished

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 new Vue({
   el: '#vue-app',
   data: {
-
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [
+      { name: 'Ryu', age: 25 },
+      { name: 'Yoshi', age: 35 },
+      { name: 'Key', age: 55 }
+    ]
   },
   methods: {
+
+  },
+  computed: {
 
   }
 });
