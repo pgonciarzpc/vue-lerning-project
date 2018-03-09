4a346a55158e37d61cd73f398d8f80db5fd170f9
Paweł <pgonciarzpc@gmail.com>
2018-03-09 13:44:35 +0100

    Fished

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-new Vue({
+var vm = new Vue({
   el: '#vue-app',
   data: {
-
+    output: 'Your fav food'
   },
   methods: {
+    readRefs: function() {
+      console.log(this.$refs.test.innerText);
 
+      this.output = this.$refs.input.value;
+    }
   }
 });
